c7d32d2a37fdcd335c761d359d2622ecda3755f2
ziyue wang <wangziyue@hotmail.co.uk>
2019-01-15 13:24:50 +0000

    nothing to see here

--- simpleModel.py
+++ simpleModel.py
@@ -40,7 +40,7 @@ def simple_model():
             
             
 		#Adding additional convolution + maxpool layers 15/1/19
-		model.add(Conv2D(32, (3,3), input_shape=(img_width,img_height,1)))
+        model.add(Conv2D(32, (3,3), input_shape=(img_width,img_height,1)))
 		model.add(Activation('relu'))
 		model.add(MaxPooling2D(pool_size=(2,2)))
 		model.add(Dropout(0.2))		
