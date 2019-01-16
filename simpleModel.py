9d1deaddbfb2bd2f0f098842282bafc234c5f67b
z <wangziyue@hotmail.co.uk>
2019-01-16 23:44:44 +0000

    Working, up to ~50% accuracy with 9 categories. More hidden conv layers seem to be doing better. Should work on image pre-preocessing though

--- simpleModel.py
+++ simpleModel.py
@@ -11,7 +11,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
-
+from keras import applications 
 
 #Writing a seed for reproducibility
 seed = 1
@@ -24,10 +24,11 @@ validation_data_dir = '128img/validation'
 nb_train_samples = 1400
 nb_validation_samples=600
 epochs=100
-batch_size=128				#Reduce this is we see problems. If using bluebear, might be smart to increase this. At home, use 128 max.
+batch_size=50				#Reduce this is we see problems. If using bluebear, might be smart to increase this. At home, use 128 max.
 
 
-img_width, img_height = 128, 128
+img_width = 128
+img_height = 128
 #input_shape=						#Input shape only needed for Conv
 
 num_pixels = img_width*img_height
@@ -41,27 +42,53 @@ def simple_model():
     #create model - custom
         
     model = Sequential()    
-	#Adding additional convolution + maxpool layers 15/1/19
-    model.add(Conv2D(64, (3,3), input_shape=(img_width,img_height,1)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.6))
-
-    model.add(Conv2D(128, (3,3)))
+    	
+        #Adding additional convolution + maxpool layers 15/1/19
+    model.add(Conv2D(32, (3,3), input_shape=(img_width,img_height,1)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.4))
 
-
+    model.add(Conv2D(64, (3,3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+    
+    model.add(Conv2D(128, (3,3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+    
     model.add(Flatten())		
 		#Possible dense layer with our 128x128 number of pixels is too much, too high. We should add a few convolutional and maxpool layers beforehand.
+    
     model.add(Dense(64,			#dimensionality of output space
 			#input_shape=(128,128,1),		#Commented out as only the first layer needs input shape. 
 		))
-    model.add(Activation('relu'))
-    #model.add(Dropout(0.2))
+    model.add(Activation('linear'))
+    model.add(Dropout(0.2))
+    
+
     model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dropout(0.2))
     model.compile(loss='categorical_crossentropy',optimizer='RMSProp',metrics=['accuracy'])
+    return model
+
+
+def vgg_model():
+    vgg16_model = applications.vgg16.VGG16(weights='imagenet',include_top=False, input_shape=(128,128,3))
+    model = Sequential()
+    for layer in vgg16_model.layers:
+        model.add(layer)
+    model.layers.pop()
+    #for layer in model.layers:          #This freezes the layers before final layer for Transfer Learning
+        #layer.trainable = False\
+    model.add(Flatten())
+    model.add(Dense(9, activation='softmax'))
+    model.summary()
+    model.compile(loss='categorical_crossentropy',optimizer='RMSProp',metrics=['accuracy'])
+    #Doesn't work
+
     return model
 
 
@@ -91,18 +118,26 @@ validation_generator = test_datagen.flow_from_directory(
 	class_mode='categorical',
 	shuffle=True)
 
-callbacks = [EarlyStopping(monitor='val_loss', patience = 10),
+callbacks = [EarlyStopping(monitor='val_loss', patience = 1),
             ModelCheckpoint(filepath='best_model.h5', monitor='acc', save_best_only=True)]
 
 
 #build the model
 model = simple_model()
 
-model.fit_generator(
+history = model.fit_generator(
 	train_generator,
 	epochs=epochs,
         callbacks=callbacks,
 	validation_data=validation_generator)
+
+acc_history = history.history['acc']
+val_acc_history = history.history['val_acc']
+
+numpy_acc_history = numpy.array(acc_history)
+numpy_val_acc_history = numpy.array(val_acc_history)
+numpy.savetxt('acc_history.txt',numpy_acc_history, delimiter=',')
+numpy.savetxt('val_acc_history.txt',numpy_val_acc_history, delimiter=',')
 
 model.save_weights('very_simple.h5')
 K.clear_session()
